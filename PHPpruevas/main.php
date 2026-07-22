b49f558235ddc781e56b4112de081b4f69419c06
Nicodox23 <perezmarxnicolas@gmail.com>
2026-07-22 15:38:37 -0300

    Agregar archivo main.php: implementar lógica básica de PHP para mostrar información de usuario, producto y cálculos matemáticos.

---
+++ PHPpruevas/main.php
@@ -0,0 +1,74 @@
+<?php
+
+$nombre = " Juan \n";
+$edad = " 41 \n";
+$ciudad = " Risvera \n";
+
+echo " nombre" . $nombre; 
+echo " edad" . $edad;
+echo " ciudad" . $ciudad;
+
+
+
+$nomproducto = " FANTA \n"; 
+$precio = " 50 \n"; 
+$cantidadisponible = " 30\n"; 
+
+echo "Nombre de producto" . $nomproducto; 
+echo "El precio es" . $precio; 
+echo "la cantidad diponible es" . $cantidadisponible; 
+
+
+
+$nombredeljugador = " Fernan \n";
+$nombrepersonaje = " SonGOKU \n";
+$nivel = " 💪: 5000 \n";
+$servidor = " 🌍: LATAM \n";
+
+echo "Nombre del jugador" . $nombredeljugador;
+echo "Nobre del personaje" . $nombrepersonaje;
+echo "Nivel" . $nivel;
+echo "Server" . $servidor; 
+
+
+
+$nombre = "Lucía \n";
+$curso = "Tercero \n";
+$materia = "Programación \n";
+
+echo "Nombre: " . $nombre;
+echo "Curso: " . $curso;
+echo "Materia: " . $materia;
+
+
+
+$numero1 = " 12 \n";
+$numero2 = " 21 \n";
+
+$Multiplicacion = $numero1 * $numero2;
+$suma = $numero1 + $numero2;
+$resta = $numero1 - $numero2;
+$division = $numero1 / $numero2; 
+
+echo "mutiplicacion" . $Multiplicacion . "\n";
+echo "suma" . $suma. "\n"; 
+echo "resta" . $resta . "\n";
+echo "division" . $division . "\n";
+
+
+$base = " 20 \n";
+$altura = " 5 \n";
+
+$area = $base * $altura; 
+
+echo "area" . $area . "\n";
+
+$Nota1 = "8";
+$Nota2 = "7";
+$Nota3 = "9";
+$Promedio =(($Nota1 + $Nota2 + $Nota3)/ 3 );
+
+echo "promedio" . $Promedio ."\n";
+
+
+?>
