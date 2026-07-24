3b36e3735ca6511e5dd6bf64b033b1e154d2b1a2
Nicodox23 <perezmarxnicolas@gmail.com>
2026-07-23 22:19:56 -0300

    Agregar lógica para comparar dos números y determinar la edad del usuario.

--- PHPpruevas/main.php
+++ PHPpruevas/main.php
@@ -1,5 +1,8 @@
 <?php
 
+
+//informacion personal
+
 $nombre = " Juan \n";
 $edad = " 41 \n";
 $ciudad = " Risvera \n";
@@ -8,7 +11,7 @@ echo " nombre" . $nombre;
 echo " edad" . $edad;
 echo " ciudad" . $ciudad;
 
-
+//informacion de un producto
 
 $nomproducto = " FANTA \n"; 
 $precio = " 50 \n"; 
@@ -18,7 +21,7 @@ echo "Nombre de producto" . $nomproducto;
 echo "El precio es" . $precio; 
 echo "la cantidad diponible es" . $cantidadisponible; 
 
-
+//informacion de un juego  
 
 $nombredeljugador = " Fernan \n";
 $nombrepersonaje = " SonGOKU \n";
@@ -30,7 +33,7 @@ echo "Nobre del personaje" . $nombrepersonaje;
 echo "Nivel" . $nivel;
 echo "Server" . $servidor; 
 
-
+//informacion personal
 
 $nombre = "Lucía \n";
 $curso = "Tercero \n";
@@ -41,6 +44,7 @@ echo "Curso: " . $curso;
 echo "Materia: " . $materia;
 
 
+//operaciones matematicas
 
 $numero1 = " 12 \n";
 $numero2 = " 21 \n";
@@ -56,6 +60,8 @@ echo "resta" . $resta . "\n";
 echo "division" . $division . "\n";
 
 
+//calculo de area de un rectangulo
+
 $base = " 20 \n";
 $altura = " 5 \n";
 
@@ -70,6 +76,8 @@ $Promedio =(($Nota1 + $Nota2 + $Nota3)/ 3 );
 
 echo "promedio" . $Promedio ."\n";
 
+
+//conversion de minutos a horas y minutos
 $minutosTotales = 135;
 $horas = (int)($minutosTotales / 60);
 $minutos = $minutosTotales % 60;
@@ -77,6 +85,58 @@ $minutos = $minutosTotales % 60;
 echo $minutosTotales . " minutos en total " ."\n";
 echo $horas . " horas  " ."\n";
 echo $minutos . " minutos" . "\n";
+
+
+//numero mayor y menor
+
+$numero1 = 10;
+$numero2 = 5;
+
+if ($numero1 == $numero2) {
+    echo "Los números son iguales\n";
+} elseif ($numero1 > $numero2) {
+    echo "El primer número es mayor\n";
+} else {
+    echo "El segundo número es mayor\n";
+}
+
+
+//edad mayor o menor
+
+
+$edad = 18;
+
+if ($edad >= 18) {
+    echo "Eres mayor de edad\n";
+} else {
+    echo "Eres menor de edad\n";
+}
+
+echo "Edad: " . $edad . "\n";
+
+
+//stock y presupuesto
+
+$stockDisponible = 10;
+$cantidadSolicitada = 3;
+$precio = 100;
+$presCliente = 500;
+
+$precioTotal = $cantidadSolicitada * $precio;
+
+if ($stockDisponible >= $cantidadSolicitada) {
+    echo "Hay stock suficiente \n";
+
+    if ($presCliente >= $precioTotal) {
+        echo "Presupesto suficiente \n";
+        echo "Compra realizada \n";
+    } else {
+        echo "insuficiente presupuesto \n";
+    }
+
+} else {
+    echo "No hay stock \n";
+}
 
 
 ?>
